801129865ce877a99516832c2a5f7e59d8ce8153
toybile <marcelino.bilibio.68@gmail.com>
2024-09-06 23:50:01 -0300

    Link - Sites

---
+++ src/js/Link - Sites.js
@@ -0,0 +1,6 @@
+document.getElementById("Border-Animation").addEventListener('click', function() {
+    window.location.href = './Link - Sites/Border Animation/index.html';
+})
+document.getElementById("Loading-Screen").addEventListener('click', function() {
+    window.location.href = './Link - Sites/Loading Screen/index.html';
+})
